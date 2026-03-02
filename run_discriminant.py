90caa597d04ed02055f16eaf6bec592833fcbf2c
Stan Selitskiy <Stanislav.Selitskiy@elnk.com>
2026-03-01 20:41:03 -0500

    Cleanup

--- run_discriminant.py
+++ run_discriminant.py
@@ -63,22 +63,22 @@ def discriminantFunct(row_num, row, mConf):
 
 if __name__ == "__main__":
     
-  DEBUG = True
+  DEBUG = False #True
     
   if DEBUG:
     mConfigIter = get_commercial_models_gen()
     mConf = next(mConfigIter)
-    #mConf = next(mConfigIter)
-    #mConf = next(mConfigIter) # free Gemini API - use for testing
-    #mConf = next(mConfigIter)
+    mConf = next(mConfigIter)
+    mConf = next(mConfigIter) # free Gemini API - use for testing
+    mConf = next(mConfigIter)
   # RUN
-  #for mConf in get_commercial_models_gen():
+  for mConf in get_commercial_models_gen():
 
     modelName = mConf.litellm_model_id.replace("/", "_")
 
     fileTpl = "haiku_translation" #"test_haiku_translation"
 
-    prevExperimentTpl = "poison" #"ablation"
+    prevExperimentTpl = "ablation" #"poison"
     inputFileTpl = f"{fileTpl}_{prevExperimentTpl}_{modelName}"
 
     experimentTpl = "discriminant"
